58da0db33225f081890a9c378c37fa66ce5519d3
yeunjukeem <139094986+yeunjukeem@users.noreply.github.com>
2024-07-16 21:09:54 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+from openai import OpenAI
+import streamlit as st
+import time
+
+# 코드스니펫 - 제목
+st.title('나만의 제품 홍보 생성기')
+
+# 코드스니펫 - 입력
+keyword = st.text_input("키워드를 입력하세요.")
+
+# 코드스니펫 - 버튼
+if st.button('생성하기'):
+  st.write('나만의 제품 홍보 카피ㅁ 완성!')
+  with st.spinner('Wait for it...'):
+
+    client = OpenAI(api_key=st.secrets["API_KEY"])
+
+    chat_completion = client.chat.completions.create(
+        messages=[{
+            "role":
+            "user",
+            "content":
+            keyword + '라는 주제로 새로운 제품을 홍보할 수 있는 카피를 150자 이내로 작성해줘',
+        }],
+        model="gpt-4o",
+    )
+
+    chat_result = chat_completion.choices[0].message.content
+    print(chat_result)
+
+    # 코드스니펫 - 글쓰기
+    st.write(chat_result)
+
+    client = OpenAI(api_key=st.secrets["API_KEY"])
+
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=keyword,
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+
+    image_url = response.data[0].url
+    # 코드스니펫 - 이미지
+    st.image(image_url)
